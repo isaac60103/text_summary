8275f23530fe2803ee7dcd46903726df6d156390
Jesse <dashmoment1017@gmail.com>
2017-04-17 17:46:38 +0800

    attentive reader

--- attentive_reader/attentive_reader.py
+++ attentive_reader/attentive_reader.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
-import numpy as np
 import model_utility as mut
-import net_factory as nf
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
@@ -11,14 +9,25 @@ mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 # Parameters
 learning_rate = 0.001
 training_iters = 10
-batch_size = 128
+
 display_step = 1
+
+batch_size = 1 #32
+sort_batch_count = 20
+n_entities = 550
+embed_size = 200
+ctx_lstm_size = 256
+question_lstm_size = 256
+attention_mlp_hidden = 100
 
 # Network Parameters
 n_input = 28 # MNIST data input (img shape: 28*28)
 n_steps = 28 # timesteps
 n_hidden = 128 # hidden layer num of features
 n_classes = 10 # MNIST total classes (0-9 digits)
+n_docout = 10 
+doc_ecode_length = 7168 
+n_doc_state = 10
 
 # tf Graph input
 inputs = tf.placeholder(tf.float32, (None, n_steps, n_input))
@@ -27,22 +36,53 @@ labels = tf.placeholder(tf.float32, (None, n_classes))
 
 
 doc_var_list = [
-            ['fcw',[2*n_hidden,n_classes]],
-            ['fcb',[n_classes]]]
-
-query_var_list = [
-            ['fcw',[2*n_hidden,n_classes]],
-            ['fcb',[n_classes]]]
+                ['d_attw',[2*ctx_lstm_size, attention_mlp_hidden]],
+                ['q_attw',[2*question_lstm_size, attention_mlp_hidden]],
+                ['wms',[attention_mlp_hidden,1]],
+                ['w_rg',[2*ctx_lstm_size, ctx_lstm_size]],
+                ['w_ug',[2*question_lstm_size, ctx_lstm_size]]
+                ]
+           
+            
 
 doc_var = mut.create_var_tnorm('document',doc_var_list)
-q_var = mut.create_var_tnorm('query',query_var_list)
 
-with tf.variable_scope("document"):
-    doc_net = nf.document_net(inputs, doc_var, n_hidden, n_steps)
+x = tf.unstack(inputs, n_steps, 1)
+
 with tf.variable_scope("query"):
-    q_net,fw,bw = nf.query_net(query, query_var_list, n_hidden,n_steps)
-
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=doc_net, labels=labels))
+    with tf.variable_scope("fw"):
+        qlstm_fw_cell = rnn.BasicLSTMCell(question_lstm_size, forget_bias=1.0)
+    # Backward direction cell
+    with tf.variable_scope("bw"):
+        qlstm_bw_cell = rnn.BasicLSTMCell(question_lstm_size, forget_bias=1.0)
+    
+    doc_net, fw, bw = rnn.static_bidirectional_rnn(qlstm_fw_cell, qlstm_bw_cell, x ,dtype=tf.float32)
+    y_q = tf.concat([fw[-1], bw[-1]],1)
+    
+    
+    
+    
+with tf.variable_scope("document"):
+    with tf.variable_scope("fw"):
+        lstm_fw_cell = rnn.BasicLSTMCell(question_lstm_size, forget_bias=1.0)
+    with tf.variable_scope("bw"):
+        lstm_bw_cell = rnn.BasicLSTMCell(question_lstm_size, forget_bias=1.0)
+        
+    docout, _, _ = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, x, dtype=tf.float32)
+    y_d = tf.concat(docout,0)
+    
+    #Attention layer
+    wy_q = tf.matmul(y_q, doc_var['q_attw']) 
+    wy_d = tf.add(tf.matmul(y_d, doc_var['d_attw']),wy_q)  
+    m_t = tf.tanh(wy_d)
+    
+    #Attention Weight
+    s_t = tf.nn.softmax(tf.transpose(tf.matmul(m_t,doc_var['wms'])))
+    
+    #Apply attention to context sum(a_t, h_i,t) --> output_dim = [1 , 2*ctx_lstm_size]
+    r =  tf.matmul(s_t, y_d)
+    
+    output = tf.tanh(tf.matmul(r,doc_var['w_rg'])  +  tf.matmul(y_q,doc_var['w_ug'])) 
 
 
 with tf.Session() as sess:
@@ -52,9 +92,12 @@ with tf.Session() as sess:
     batch_x, batch_y = mnist.train.next_batch(batch_size)
     batch_x = batch_x.reshape((batch_size, n_steps, n_input))
      
-    doc_out = sess.run(doc_net, feed_dict={inputs: batch_x, labels: batch_y})
-    out= sess.run(q_net, feed_dict={query: batch_x})
-    outfw = sess.run(fw, feed_dict={query: batch_x})
+
+    pre = sess.run(output, feed_dict={inputs: batch_x})
+    
 
     
+
+    
+   
     
--- attentive_reader/dirtytest.py
+++ attentive_reader/dirtytest.py
@@ -17,62 +17,78 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 
 # Parameters
-learning_rate = 0.001
-training_iters = 200
-batch_size = 50
-display_step = 5
+#learning_rate = 0.001
+#training_iters = 200
+#batch_size = 50
+#display_step = 5#
 
-# Network Parameters
-n_input = 28 # MNIST data input (img shape: 28*28)
-n_steps = 28 # timesteps
-n_hidden = 128 # hidden layer num of features
-n_classes = 10 # MNIST total classes (0-9 digits)
+## Network Parameters
+#n_input = 28 # MNIST data input (img shape: 28*28)
+#n_steps = 28 # timesteps
+#n_hidden = 128 # hidden layer num of features
+#n_classes = 10 # MNIST total classes (0-9 digits)#
 
-# tf Graph input
-x = tf.placeholder("float", [None, n_steps, n_input])
-y = tf.placeholder("float", [None, n_classes])
+## tf Graph input
+#x = tf.placeholder("float", [None, n_steps, n_input])
+#y = tf.placeholder("float", [None, n_classes])#
 
-# Define weights
-with tf.variable_scope("test", reuse=True):
-    weights2 = {
-        # Hidden layer weights => 2*n_hidden because of forward + backward cells
-        'out': tf.Variable(tf.random_normal([2*n_hidden, n_classes]))
-    }
-    biases2 = {
-        'out': tf.Variable(tf.random_normal([n_classes]))
-    }
+## Define weights
+#with tf.variable_scope("test", reuse=True):
+#    weights2 = {
+#        # Hidden layer weights => 2*n_hidden because of forward + backward cells
+#        'out': tf.Variable(tf.random_normal([2*n_hidden, n_classes]))
+#    }
+#    biases2 = {
+#        'out': tf.Variable(tf.random_normal([n_classes]))
+#    }#
+#
+
+#b_x = tf.unstack(x, n_steps, 1)
+#lstm_fw_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
+#lstm_bw_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
+#outputs, lf, lb = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, b_x, dtype=tf.float32)
+#pred = tf.matmul(outputs[-1], weights2['out']) + biases2['out']
+#f_o = lf[-1]
+#lb_o = lb[-1]#
+#
+
+#init = tf.global_variables_initializer()
+#with tf.Session() as sess:
+#    sess.run(init)
+#    step = 1
+#    # Keep training until reach max iterations
+#    while step * batch_size < training_iters:
+#        batch_x, batch_y = mnist.train.next_batch(batch_size)
+#        # Reshape data to get 28 seq of 28 elements
+#        batch_x = batch_x.reshape((batch_size, n_steps, n_input))
+#        
+#        backward = sess.run(lb_o, feed_dict={x: batch_x}) #backward = tumple of [c_state, m_state]: c_state is cell state, m_state is output(hidden) state
+#        forward = sess.run(f_o, feed_dict={x: batch_x})
+#        pred = sess.run(outputs, feed_dict={x: batch_x})
+#        
+#        step = step + 1
+        
+        
+        
+        
 
 
-b_x = tf.unstack(x, n_steps, 1)
-lstm_fw_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-lstm_bw_cell = rnn.BasicLSTMCell(n_hidden, forget_bias=1.0)
-outputs, lf, lb = rnn.static_bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, b_x, dtype=tf.float32)
-pred = tf.matmul(outputs[-1], weights2['out']) + biases2['out']
-f_o = lf[-1]
-lb_o = lb[-1]
-
-
-init = tf.global_variables_initializer()
-with tf.Session() as sess:
-    sess.run(init)
-    step = 1
-    # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        batch_x, batch_y = mnist.train.next_batch(batch_size)
-        # Reshape data to get 28 seq of 28 elements
-        batch_x = batch_x.reshape((batch_size, n_steps, n_input))
-        
-        backward = sess.run(lb_o, feed_dict={x: batch_x}) #backward = tumple of [c_state, m_state]: c_state is cell state, m_state is output(hidden) state
-        forward = sess.run(f_o, feed_dict={x: batch_x})
-        pred = sess.run(outputs, feed_dict={x: batch_x})
-        
-        step = step + 1
-        
-        
-        
-        
-        
-        
+X = np.random.randn(2, 10, 8)
+ 
+X[1,6,:] = 0
+X_lengths = [10, 6]
+ 
+cell = tf.nn.rnn_cell.LSTMCell(num_units=64, state_is_tuple=True)
+ 
+outputs, states  = tf.nn.bidirectional_dynamic_rnn(
+    cell_fw=cell,
+    cell_bw=cell,
+    dtype=tf.float64,
+    sequence_length=X_lengths,
+    inputs=X)
+ 
+output_fw, output_bw = outputs
+states_fw, states_bw = states    
         
         
         
